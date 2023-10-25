0f229fd08a0d8f67471dd5a7978f4eb573bec85f
quivo <ngocquivo11@gmail.com>
2023-10-25 23:06:22 +0700

    fix csproj

--- DoAnStarbucks/Promotion.Designer.cs
+++ DoAnStarbucks/Promotion.Designer.cs
@@ -49,81 +49,93 @@
             // lbKhuyenMai
             // 
             this.lbKhuyenMai.AutoSize = true;
-            this.lbKhuyenMai.Location = new System.Drawing.Point(75, 117);
+            this.lbKhuyenMai.Location = new System.Drawing.Point(56, 95);
+            this.lbKhuyenMai.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbKhuyenMai.Name = "lbKhuyenMai";
-            this.lbKhuyenMai.Size = new System.Drawing.Size(76, 16);
+            this.lbKhuyenMai.Size = new System.Drawing.Size(63, 13);
             this.lbKhuyenMai.TabIndex = 1;
             this.lbKhuyenMai.Text = "Khuyến Mãi";
             // 
             // lbTenKhuyenMai
             // 
             this.lbTenKhuyenMai.AutoSize = true;
-            this.lbTenKhuyenMai.Location = new System.Drawing.Point(75, 159);
+            this.lbTenKhuyenMai.Location = new System.Drawing.Point(56, 129);
+            this.lbTenKhuyenMai.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbTenKhuyenMai.Name = "lbTenKhuyenMai";
-            this.lbTenKhuyenMai.Size = new System.Drawing.Size(103, 16);
+            this.lbTenKhuyenMai.Size = new System.Drawing.Size(85, 13);
             this.lbTenKhuyenMai.TabIndex = 1;
             this.lbTenKhuyenMai.Text = "Tên Khuyến Mãi";
             // 
             // lbBatDau
             // 
             this.lbBatDau.AutoSize = true;
-            this.lbBatDau.Location = new System.Drawing.Point(75, 201);
+            this.lbBatDau.Location = new System.Drawing.Point(56, 163);
+            this.lbBatDau.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbBatDau.Name = "lbBatDau";
-            this.lbBatDau.Size = new System.Drawing.Size(90, 16);
+            this.lbBatDau.Size = new System.Drawing.Size(74, 13);
             this.lbBatDau.TabIndex = 1;
             this.lbBatDau.Text = "Ngày Bắt Đầu";
             // 
             // lbKetThuc
             // 
             this.lbKetThuc.AutoSize = true;
-            this.lbKetThuc.Location = new System.Drawing.Point(75, 250);
+            this.lbKetThuc.Location = new System.Drawing.Point(56, 203);
+            this.lbKetThuc.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbKetThuc.Name = "lbKetThuc";
-            this.lbKetThuc.Size = new System.Drawing.Size(98, 16);
+            this.lbKetThuc.Size = new System.Drawing.Size(82, 13);
             this.lbKetThuc.TabIndex = 1;
             this.lbKetThuc.Text = "Ngày Kết Thúc ";
             // 
             // tbKhuyenMai
             // 
-            this.tbKhuyenMai.Location = new System.Drawing.Point(184, 117);
+            this.tbKhuyenMai.Location = new System.Drawing.Point(138, 95);
+            this.tbKhuyenMai.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbKhuyenMai.Name = "tbKhuyenMai";
-            this.tbKhuyenMai.Size = new System.Drawing.Size(200, 22);
+            this.tbKhuyenMai.Size = new System.Drawing.Size(151, 20);
             this.tbKhuyenMai.TabIndex = 2;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(184, 291);
+            this.textBox2.Location = new System.Drawing.Point(138, 236);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(200, 22);
+            this.textBox2.Size = new System.Drawing.Size(151, 20);
             this.textBox2.TabIndex = 2;
             // 
             // tbTenKhuyenMai
             // 
-            this.tbTenKhuyenMai.Location = new System.Drawing.Point(184, 156);
+            this.tbTenKhuyenMai.Location = new System.Drawing.Point(138, 127);
+            this.tbTenKhuyenMai.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbTenKhuyenMai.Name = "tbTenKhuyenMai";
-            this.tbTenKhuyenMai.Size = new System.Drawing.Size(200, 22);
+            this.tbTenKhuyenMai.Size = new System.Drawing.Size(151, 20);
             this.tbTenKhuyenMai.TabIndex = 2;
             // 
             // lbMieuTa
             // 
             this.lbMieuTa.AutoSize = true;
-            this.lbMieuTa.Location = new System.Drawing.Point(84, 297);
+            this.lbMieuTa.Location = new System.Drawing.Point(63, 241);
+            this.lbMieuTa.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbMieuTa.Name = "lbMieuTa";
-            this.lbMieuTa.Size = new System.Drawing.Size(56, 16);
+            this.lbMieuTa.Size = new System.Drawing.Size(46, 13);
             this.lbMieuTa.TabIndex = 1;
             this.lbMieuTa.Text = "Miêu Tả";
             // 
             // dtpBatDau
             // 
-            this.dtpBatDau.Location = new System.Drawing.Point(184, 195);
+            this.dtpBatDau.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpBatDau.Location = new System.Drawing.Point(138, 158);
+            this.dtpBatDau.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dtpBatDau.Name = "dtpBatDau";
-            this.dtpBatDau.Size = new System.Drawing.Size(200, 22);
+            this.dtpBatDau.Size = new System.Drawing.Size(151, 20);
             this.dtpBatDau.TabIndex = 3;
             // 
             // dtpKetThuc
             // 
-            this.dtpKetThuc.Location = new System.Drawing.Point(184, 244);
+            this.dtpKetThuc.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpKetThuc.Location = new System.Drawing.Point(138, 198);
+            this.dtpKetThuc.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dtpKetThuc.Name = "dtpKetThuc";
-            this.dtpKetThuc.Size = new System.Drawing.Size(200, 22);
+            this.dtpKetThuc.Size = new System.Drawing.Size(151, 20);
             this.dtpKetThuc.TabIndex = 3;
             // 
             // listView1
@@ -135,9 +147,10 @@
             this.columnHeader4,
             this.columnHeader5});
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(524, 65);
+            this.listView1.Location = new System.Drawing.Point(393, 53);
+            this.listView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(486, 368);
+            this.listView1.Size = new System.Drawing.Size(366, 300);
             this.listView1.TabIndex = 4;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
@@ -168,9 +181,9 @@
             // 
             // Promotion
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1099, 559);
+            this.ClientSize = new System.Drawing.Size(824, 454);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.dtpKetThuc);
             this.Controls.Add(this.dtpBatDau);
@@ -182,6 +195,7 @@
             this.Controls.Add(this.lbBatDau);
             this.Controls.Add(this.lbTenKhuyenMai);
             this.Controls.Add(this.lbKhuyenMai);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Promotion";
             this.Text = "Promotion";
             this.ResumeLayout(false);
